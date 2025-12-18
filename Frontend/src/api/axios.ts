2fe1db6d16da86603da7f404eb111f5be1d90b57
riyasharma <riya.sharma@codingworkx.com>
2025-12-18 17:32:54 +0530

    deleted Frontend

--- Frontend/src/api/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://task-management-2-aad9.onrender.com/api/v1",
-  withCredentials: true,
-});
-
-export default api;
--- Frontend/src/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-export const socket = io(
-  "https://task-management-2-aad9.onrender.com",
-  {
-    withCredentials: true,
-  }
-);
